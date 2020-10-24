5a2d0039903ab6fe585d05a93b2c6bb3a47b3bc5
Begemotas <cheminiselementas@gmail.com>
2020-10-24 23:36:17 +0300

    final score in % aded

--- my-app/src/components/Home/Home.js
+++ my-app/src/components/Home/Home.js
@@ -1,15 +1,12 @@
 import React from 'react';
-//import homePhoto from '../assets/img/landing_page.jpg';
 import './Home.scss';
 import Navbar from '../Navbar/Navbar';
-import TestFinished from '../TestFinished/TestFinished';
 
 export const home=()=>{
     
         return (
             <div class="Home">
                 <Navbar/>
-                {/* <TestFinished/> */}
             </div>
         )
     
